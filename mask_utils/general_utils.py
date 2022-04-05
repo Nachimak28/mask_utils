669d37fe38ddaffeb4a2e2cba2227e16b9cd7ec5
Nachiket <nachimak28@gmail.com>
2022-04-05 17:28:15 +0530

    added b64 to mask and vice-versa functions

--- mask_utils/general_utils.py
+++ mask_utils/general_utils.py
@@ -1,4 +1,8 @@
 import os
+import cv2
+import zlib
+import base64
+import io as bio
 import numpy as np
 import imageio as io
 from PIL import Image, ImageFile
@@ -73,11 +77,21 @@ def rle_to_mask(rle_string, height, width, max_intensity=255):
     mask = mask.T
     return mask
 
-def mask_to_base64():
-    pass
+def mask_to_base64(mask):
+    img_pil = Image.fromarray(np.array(mask, dtype=np.uint8))
+    img_pil.putpalette([0,0,0,255,255,255])
+    bytes_io = bio.BytesIO()
+    img_pil.save(bytes_io, format='PNG', transparency=0, optimize=0)
+    bytes = bytes_io.getvalue()
+    return base64.b64encode(zlib.compress(bytes)).decode('utf-8')
 
-def base64_to_mask():
-    pass
+def base64_to_mask(bstring):
+    z = zlib.decompress(base64.b64decode(s))
+    n = np.fromstring(z, np.uint8)
+    mask = cv2.imdecode(n, cv2.IMREAD_UNCHANGED)[:, :, 3].astype(bool)
+    mask = np.asarray(mask, dtype=np.uint8)*255
+    return mask
+    
 
 def feather_mask():
     pass
