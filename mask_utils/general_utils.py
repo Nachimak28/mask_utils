8caf442ef9b4be5dffe562baf39e3a8aacf3e913
Nachiket <nachimak28@gmail.com>
2022-04-16 11:00:05 +0530

    updated b64 related functions

--- mask_utils/general_utils.py
+++ mask_utils/general_utils.py
@@ -1,4 +1,3 @@
-from enum import unique
 import os
 import cv2
 import zlib
@@ -80,17 +79,14 @@ def rle_to_mask(rle_string, height, width, max_intensity=255):
 
 def mask_to_base64(mask):
     img_pil = Image.fromarray(np.array(mask, dtype=np.uint8))
-    img_pil.putpalette([0,0,0,255,255,255])
-    bytes_io = bio.BytesIO()
-    img_pil.save(bytes_io, format='PNG', transparency=0, optimize=0)
-    bytes = bytes_io.getvalue()
-    return base64.b64encode(zlib.compress(bytes)).decode('utf-8')
+    buffer = bio.BytesIO()
+    img_pil.save(buffer, format='PNG', transparency=0, optimize=0)
+    return base64.b64encode(buffer.getvalue()).decode('utf-8')
 
 def base64_to_mask(bstring):
-    z = zlib.decompress(base64.b64decode(s))
-    n = np.fromstring(z, np.uint8)
-    mask = cv2.imdecode(n, cv2.IMREAD_UNCHANGED)[:, :, 3].astype(bool)
-    mask = np.asarray(mask, dtype=np.uint8)*255
+    decoded_data = base64.b64decode(bstring)
+    pil_mask = Image.open(bio.BytesIO(decoded_data)).convert('L')
+    mask = np.asarray(pil_mask, dtype=np.uint8)
     return mask
 
 
